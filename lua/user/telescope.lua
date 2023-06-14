dd9237d155e723ed1089a29b3e1b37b6265894c9
bel8 <68114296+bart-b8@users.noreply.github.com>
2023-06-14 21:12:08 +0200

    Initial commit

---
+++ lua/user/telescope.lua
@@ -0,0 +1,33 @@
+local M = {
+  "nvim-telescope/telescope.nvim",
+  commit = "40c31fdde93bcd85aeb3447bb3e2a3208395a868",
+  event = "Bufenter",
+  cmd = { "Telescope" },
+  dependencies = {
+    {
+      "ahmedkhalf/project.nvim",
+      commit = "8c6bad7d22eef1b71144b401c9f74ed01526a4fb",
+    },
+  },
+}
+
+local actions = require "telescope.actions"
+
+M.opts = {
+  defaults = {
+    prompt_prefix = " ",
+    selection_caret = " ",
+    path_display = { "smart" },
+    file_ignore_patterns = { ".git/", "node_modules" },
+    mappings = {
+      i = {
+        ["<Down>"] = actions.move_selection_next,
+        ["<Up>"] = actions.move_selection_previous,
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+      },
+    },
+  },
+}
+
+return M
