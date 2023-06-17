f74d19896aaa627dd1be9ac41b8481c3d26bfb1e
Bart Decoutere <bart.decoutere@ugent.be>
2023-06-17 16:13:06 +0200

    adds <leader>fh

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -63,6 +63,7 @@ keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
 keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
 keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
 keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
+keymap("n", "<leader>fh", ":Telescope help_tags<CR>", opts)
 
 -- Git
 keymap("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
